9f63228677bac716634f3118c20744a78dc2f0a9
Austiel <abdielaustero.work@gmail.com>
2025-07-03 10:33:59 +0800

    fix

--- config/db.php
+++ config/db.php
@@ -1,8 +1,13 @@
 <?php
-$host = "tk-webapp.cdy2k86gkg9l.ap-southeast-2.rds.amazonaws.com";
-$user = "admin";
-$pass = "xDLmjlGtvflLYlkottKJ";
-$dbname = "tk_webapp";
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
